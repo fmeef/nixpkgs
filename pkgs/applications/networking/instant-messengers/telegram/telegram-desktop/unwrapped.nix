739de7e52c6b39b139de8dd90abf2c94405b2a05
fmeef <gnu3ra@riseup.net>
2026-04-04 00:43:09 -0500

    update telegram-desktop to 6.7.2

--- pkgs/applications/networking/instant-messengers/telegram/telegram-desktop/unwrapped.nix
+++ pkgs/applications/networking/instant-messengers/telegram/telegram-desktop/unwrapped.nix
@@ -45,14 +45,14 @@
 
 stdenv.mkDerivation (finalAttrs: {
   pname = "telegram-desktop-unwrapped";
-  version = "6.7.1";
+  version = "6.7.2";
 
   src = fetchFromGitHub {
     owner = "telegramdesktop";
     repo = "tdesktop";
     rev = "v${finalAttrs.version}";
     fetchSubmodules = true;
-    hash = "sha256-XDgz0i4AIrJUZY/5BVP+1uROW8ePRmmZ8aXuPrRdO+s=";
+    hash = "sha256-8P5M/dXO08+LQeRtfta6OSov0LOb4b2xELeowueHDZ0=";
   };
 
   nativeBuildInputs = [
