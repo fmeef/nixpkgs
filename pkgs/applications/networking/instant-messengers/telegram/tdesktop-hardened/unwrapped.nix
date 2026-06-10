98b3aabf792e2dee1d2995988824c2907e4e7da5
fmeef <gnu3ra@riseup.net>
2026-06-10 02:36:54 -0500

    bump tdesktop-hardened to v0.0.6

--- pkgs/applications/networking/instant-messengers/telegram/tdesktop-hardened/unwrapped.nix
+++ pkgs/applications/networking/instant-messengers/telegram/tdesktop-hardened/unwrapped.nix
@@ -21,6 +21,7 @@
   openal-soft,
   minizip-ng,
   range-v3,
+  minizip,
   tl-expected,
   hunspell,
   gobject-introspection,
@@ -34,6 +35,7 @@
   libicns,
   apple-sdk_15,
   nix-update-script,
+  kdePackages
 }:
 
 # Main reference:
@@ -45,14 +47,14 @@
 
 stdenv.mkDerivation (finalAttrs: {
   pname = "tdesktop-hardened-unwrapped";
-  version = "0.0.5";
+  version = "0.0.6";
 
   src = fetchFromGitHub {
     owner = "fmeef";
     repo = "tdesktop-hardened";
     rev = "v${finalAttrs.version}";
     fetchSubmodules = true;
-    hash = "sha256-c54JvAeZHq0YLq/PV8G0tt9EEKf64XZP8H4hjLTN1mg=";
+    hash = "sha256-5DMzNqqW9If4VsH7yxjRYAJeqZIo2bKKMuz0M3mwA9s";
   };
 
   nativeBuildInputs = [
@@ -76,10 +78,12 @@ stdenv.mkDerivation (finalAttrs: {
     openal-soft
     minizip-ng
     range-v3
+    minizip
     tl-expected
     rnnoise
     tg_owt
     microsoft-gsl
+    kdePackages.qtshadertools
     boost
     ada
     (tdlib.override { tde2eOnly = true; })
