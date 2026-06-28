636069e1a6523a5894b4b2b56f01015edc990409
fmeef <gnu3ra@riseup.net>
2026-06-28 05:24:45 -0500

    update tdesktop-hardened

--- pkgs/applications/networking/instant-messengers/telegram/tdesktop-hardened/unwrapped.nix
+++ pkgs/applications/networking/instant-messengers/telegram/tdesktop-hardened/unwrapped.nix
@@ -47,14 +47,14 @@
 
 stdenv.mkDerivation (finalAttrs: {
   pname = "tdesktop-hardened-unwrapped";
-  version = "0.0.6";
+  version = "0.0.7";
 
   src = fetchFromGitHub {
     owner = "fmeef";
     repo = "tdesktop-hardened";
     rev = "v${finalAttrs.version}";
     fetchSubmodules = true;
-    hash = "sha256-5DMzNqqW9If4VsH7yxjRYAJeqZIo2bKKMuz0M3mwA9s";
+    hash = "sha256-HP9mluEmIhao3p1uhOLBbquSahsSv2Ph4PcNu4AloeM=";
   };
 
   nativeBuildInputs = [
